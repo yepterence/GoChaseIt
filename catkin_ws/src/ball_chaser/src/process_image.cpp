f5cdb2e5137da7c9d8061423146a71150bca2f7e
yepterence <terence.yep@gmail.com>
2019-08-05 15:42:41 -0400

    Added r,g,b pixel value tracking, improved directional tracking of white ball, tracking white pixel position

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -29,82 +29,79 @@ void process_image_callback(const sensor_msgs::Image img)
 
     // Declaring variables
     int red_pixel, green_pixel, blue_pixel, i, row;
-    int left_divider = img.step / 3;
-    int right_divider = left_divider * 2;
-    //Set initial ball existance
-    bool ball_detection = false;
-    
+        
     // define movement speed and turning speed in radians
     float forward_move = 1.0;
-    float turn_speed = 0.25;
+    
     float stop = 0.0;
 
     //Turning speeds
-    float turn_left = -2.0;
-    float turn_right = 2.0;
+    float turn_left = -0.75;
+    float turn_right = 0.75;
 
     // Loop through each pixel in the image and check if there's a bright white one
     // 
     
-    for (int i=0;i < img.height * img.step; ++i)
+    
+
+    for (int i=0;i < img.height * img.step; i += 3)
     {   
-        
+
         int red_pixel = img.data[i];
         int green_pixel = img.data[i+1];
         int blue_pixel = img.data[i+2];
+        int pixel_position = i % (img.width * 3) / 3;
 
-        if (i%img.step < left_divider && red_pixel == 255 && green_pixel == 255 && blue_pixel == 255)
-        {
-        
-            ROS_INFO("Target acquired");
-            
-            ROS_INFO("Target is left, driving left");
-            drive_robot(stop,turn_left);
-            // drive_robot(turn_left,turn_speed);
-        }
-            
-        else if (i%img.step > right_divider && red_pixel == 255 && green_pixel == 255 && blue_pixel == 255)
-        {
-            ROS_INFO("Target is right, driving right");
-            drive_robot(stop,turn_right);
-            // drive_robot(turn_right,turn_speed);
-        }
-        
-        else if (red_pixel == 255 && green_pixel == 255 && blue_pixel == 255)
-        {
-            ROS_INFO("Target straight ahead, proceed forward");
-            drive_robot(forward_move,stop);
-        }
+        // left and right divider values were determined based on observations on width outputs
+        // when ball was detected
+        int left_divider = 265;
+        int right_divider = 533;
 
-    // Request a stop when there's no white ball seen by the camera
-        else
+
+        if (red_pixel == 255 && green_pixel == 255 && blue_pixel == 255)
         {
-            ROS_INFO("Target lost, standing by");
-            drive_robot(stop,stop);
-        }
+            
+            ROS_INFO("Found white ball");
+            // std::cout<<"at position index: "<<pixel_position;
+            // std::cout<<"found red: \n" << red_pixel;
+
+            if (pixel_position >1 && pixel_position <= left_divider)
+            {
+                // std::cout<<"Found white ball in Left";
+                ROS_INFO("Target is left, driving left");
+                drive_robot(stop,turn_left);
+                if (pixel_position >left_divider && pixel_position <= right_divider)
+                {
+                    ROS_INFO("Pixel is now centered");
+                    drive_robot(stop,stop);
+                }
+                
+            }
+            else if (pixel_position > right_divider && pixel_position < img.width)
+            {
+                
+                ROS_INFO("Target is right, driving right");
+                drive_robot(stop,turn_right);
+
+                if (pixel_position >left_divider && pixel_position <= right_divider)
+                {
+                    ROS_INFO("Pixel is now centered");
+                    drive_robot(stop,stop);
+                    ROS_INFO("position: %f", pixel_position);        
+                }
+                
+            }
+            else if (pixel_position >left_divider && pixel_position <= right_divider)
+            {
+                ROS_INFO("Target straight ahead, proceed forward");                                     
+                drive_robot(forward_move,stop);
+               
+            }
+        }     
     }
+     
 }
-        // else
-        // {
-        // 	int mean_scan_position = scan_position_sum / white_pixel_count;
-        // 	// Check if pixels are on left frame
-        // 	if (mean_scan_position < img.width /3)
-        // 	{
-        // 		ROS_INFO("turning left, moving forward");
-        // 		drive_robot(forward_move, turn_left);
-
-        // 	}
-        // 	// Check if pixels are on right frame
-        // 	else if (mean_scan_position > img.width * 2/3)
-        // 	{
-        // 		ROS_INFO("turning right, moving forward");
-        // 		drive_robot(forward_move, turn_right);
-        // 	}
-        // 	else
-        // 	{
-        // 		ROS_INFO("Target ahead, driving forward");
-        // 		drive_robot(forward_move, 0.0);
-        // 	}
+       
 
 
 
@@ -118,7 +115,7 @@ int main(int argc, char** argv)
     client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
 
     // Subscribe to /camera/rgb/image_raw topic to read the image data inside the process_image_callback function
-    ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 10, process_image_callback);
+    ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 2, process_image_callback);
 
     // Handle ROS communication events
     ros::spin();
